ed9fde5dfff3d2b56731500c2f2b78a2aa0f6e28
Danny <danny@lilytronics.nl>
2024-04-19 15:54:15 +0200

    add script to replace text in a file

---
+++ scripts/replace_in_file.py
@@ -0,0 +1,13 @@
+"""
+Replaces a section for another section in the whole file.
+"""
+
+search = ''''''
+replace = ''''''
+filename = "../symbols/lily_symbols.kicad_sym"
+
+with open(filename, "r") as fp:
+    content = fp.read()
+
+with open(filename, "w") as fp:
+    fp.write(content.replace(search, replace))
