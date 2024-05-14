68f10dc2158a2adea91ae606daa73ae2de4f4b49
Danny <danny@lilytronics.nl>
2024-05-14 08:01:26 +0200

    add check for footprints

--- scripts/check_library.py
+++ scripts/check_library.py
@@ -7,9 +7,17 @@ import os
 from lib_parser import LibParser
 
 
+#############
+# Log error #
+#############
+
 def _log_error(identifier, message):
     print(f"{identifier}: {message}")
 
+
+#################
+# Symbol checks #
+#################
 
 def _check_reference(symbol_data):
     is_correct = False
@@ -43,7 +51,7 @@ def _check_reference(symbol_data):
         _log_error(symbol_data["Name"], "incorrect reference")
 
 
-def _check_empty_field(symbol_data, field_name):
+def _check_symbol_field_empty(symbol_data, field_name):
     # Ignore fields that are allowed to be empty or have already been checked
     if field_name not in ["Name", "Datasheet", "Description", "Reference", "Revision", "Notes", "Extends"]:
         field_checked = False
@@ -64,7 +72,7 @@ def _check_empty_field(symbol_data, field_name):
                 # Parts
                 is_empty = symbol_data[field_name] == ""
         if not field_checked:
-            _log_error(symbol_data["Name"], f"no is empty check for this field '{field_name}'")
+            _log_error(symbol_data["Name"], f"no empty check for this field '{field_name}'")
         if is_empty:
             _log_error(symbol_data["Name"], f"field '{field_name}' is empty")
         if is_not_empty:
@@ -88,23 +96,179 @@ def _check_footprint(symbol_data):
 
 def check_symbols():
     symbols = LibParser.get_symbols()
+    print(f"Checking {len(symbols)} symbols")
     for symbol in symbols:
         try:
             revision = int(symbol["Revision"])
             if revision < 1:
                 _log_error(symbol["Name"], "the revision must be greater than zero")
-        except ValueError:
+        except (TypeError, ValueError):
             _log_error(symbol["Name"], "the revision must be numeric")
         if symbol["Extends"] == "":
             if symbol["Name"] != symbol["Value"]:
                 _log_error(symbol["Name"], "the value is not the same as the name")
         _check_reference(symbol)
         for field in symbol:
-            _check_empty_field(symbol, field)
+            _check_symbol_field_empty(symbol, field)
         if symbol["Footprint"] != "":
             _check_footprint(symbol)
+
+
+####################
+# Footprint checks #
+####################
+
+def _check_footprint_field_empty(footprint_data, field_name):
+    # Ignore fields that are allowed to be empty or have already been checked
+    if field_name not in ["Name", "Datasheet", "Description", "Footprint", "Revision", "Attributes", "Reference_F.Fab"]:
+        field_checked = False
+        is_empty = False
+        is_not_empty = False
+        value = footprint_data[field_name]
+        if isinstance(value, dict):
+            value = value["Value"]
+        # Always must contain a value
+        if field_name in ["Reference", "Value"]:
+            field_checked = True
+            is_empty = value == ""
+        elif field_name == "Model":
+            field_checked = True
+            # Some footprints must not have a 3D model
+            if (footprint_data["Name"].endswith("_dnp") or
+                    footprint_data["Name"].startswith("test_point_") or
+                    footprint_data["Name"].startswith("fiducial_") or
+                    footprint_data["Name"].startswith("logo_") or
+                    footprint_data["Name"].startswith("mec_")):
+                is_not_empty = value != ""
+            else:
+                is_empty = value == ""
+        if not field_checked:
+            _log_error(footprint_data["Name"], f"no empty check for this field '{field_name}'")
+        if is_empty:
+            _log_error(footprint_data["Name"], f"field '{field_name}' is empty")
+        if is_not_empty:
+            _log_error(footprint_data["Name"], f"field '{field_name}' is not empty")
+
+
+def _check_footprint_field_properties(footprint_data, field_name):
+    # Check layer, size, thickness of the field
+    if field_name not in ["Name", "Model"]:
+        if isinstance(footprint_data[field_name], dict):
+            field_checked = [False, False]
+
+            # field_checked[0]: visibility
+            expect_visible = field_name in ["Reference", "Reference_F.Fab"]
+            if (footprint_data["Name"].startswith("fiducial_") or
+                    footprint_data["Name"].startswith("logo_") or
+                    footprint_data["Name"].startswith("mec_")):
+                expect_visible = False
+            if footprint_data["Name"].startswith("test_point") and field_name == "Value":
+                expect_visible = True
+            field_checked[0] = True
+            if not footprint_data[field_name]["Visible"] and expect_visible:
+                _log_error(footprint_data["Name"], f"field '{field_name}' is not visible")
+            if footprint_data[field_name]["Visible"] and not expect_visible:
+                _log_error(footprint_data["Name"], f"field '{field_name}' should not be visible")
+
+            property_to_checks = {
+                # Field name: (layer, size, thickness)
+                "Reference": ("F.SilkS", "1 1", "0.15"),
+                "Value": ("F.Fab", "0.5 0.5", "0.1"),
+                "Footprint": ("F.Fab", "1.27 1.27", ""),
+                "Datasheet": ("F.Fab", "1.27 1.27", ""),
+                "Description": ("F.Fab", "1.27 1.27", ""),
+                "Revision": ("F.Fab", "0.5 0.5", "0.1"),
+                "Reference_F.Fab": ("F.Fab", "0.5 0.5", "0.1"),
+            }
+            if footprint_data["Name"].startswith("test_point_"):
+                property_to_checks["Value"] = ("F.SilkS", "1 1", "0.15")
+            if field_name in property_to_checks:
+                field_checked[1] = True
+                # Layer
+                if footprint_data[field_name]["Layer"] != property_to_checks[field_name][0]:
+                    _log_error(footprint_data["Name"],
+                               f"field '{field_name}' should be on layer {property_to_checks[field_name][0]}")
+                    print(footprint_data)
+                # Size
+                if footprint_data[field_name]["Size"] != property_to_checks[field_name][1]:
+                    _log_error(footprint_data["Name"],
+                               f"field '{field_name}' size should be {property_to_checks[field_name][1]}")
+                # Thickness
+                if footprint_data[field_name]["Thickness"] != property_to_checks[field_name][2]:
+                    _log_error(footprint_data["Name"],
+                               f"field '{field_name}' thickness should be {property_to_checks[field_name][2]}")
+
+            if False in field_checked:
+                _log_error(footprint_data["Name"], f"not all checks implemented for this field '{field_name}'")
+                print(footprint_data["Name"], field_name, footprint_data[field_name])
+
+
+def _check_footprint_attributes(footprint_data):
+    #print(footprint_data["Name"], footprint_data["Attributes"])
+
+    # Default we expect no attributes to be enabled
+    expected_attributes = {
+        "board_only": [False, "not in schematic"],
+        "exclude_from_pos_files": [False, "exclude from position files"],
+        "exclude_from_bom": [False, "exclude from bill of materials"],
+        "allow_missing_courtyard": [False, "exempt from courtyard requirement"],
+        "dnp": [False, "do not populate"]
+    }
+
+    if "through_hole" in footprint_data["Attributes"]:
+        expected_attributes["exclude_from_pos_files"][0] = True
+    if footprint_data["Name"].endswith("_dnp"):
+        expected_attributes["exclude_from_pos_files"][0] = True
+        expected_attributes["exclude_from_bom"][0] = True
+        expected_attributes["dnp"][0] = True
+    if footprint_data["Name"].startswith("fiducial_"):
+        expected_attributes["board_only"][0] = True
+        expected_attributes["exclude_from_bom"][0] = True
+    if footprint_data["Name"].startswith("logo_"):
+        expected_attributes["board_only"][0] = True
+        expected_attributes["exclude_from_pos_files"][0] = True
+        expected_attributes["exclude_from_bom"][0] = True
+        expected_attributes["allow_missing_courtyard"][0] = True
+    if footprint_data["Name"].startswith("mec_"):
+        expected_attributes["board_only"][0] = True
+        expected_attributes["exclude_from_pos_files"][0] = True
+        expected_attributes["exclude_from_bom"][0] = True
+    if footprint_data["Name"].startswith("test_point_"):
+        expected_attributes["exclude_from_pos_files"][0] = True
+        expected_attributes["exclude_from_bom"][0] = True
+
+    for attribute in expected_attributes:
+        if attribute in footprint_data["Attributes"] and not expected_attributes[attribute][0]:
+            _log_error(footprint_data["Name"], f"attribute {expected_attributes[attribute][1]} should not be enabled")
+        elif attribute not in footprint_data["Attributes"] and expected_attributes[attribute][0]:
+            _log_error(footprint_data["Name"], f"attribute {expected_attributes[attribute][1]} should be enabled")
+
+
+def check_footprints():
+    footprints = LibParser.get_footprints()
+    print(f"Checking {len(footprints)} footprints")
+    for footprint in footprints:
+        try:
+            revision = int(footprint["Revision"]["Value"])
+            if revision < 1:
+                _log_error(footprint["Name"], "the revision must be greater than zero")
+        except (TypeError, ValueError):
+            _log_error(footprint["Name"], "the revision must be numeric")
+
+        for field in footprint:
+            _check_footprint_field_empty(footprint, field)
+            _check_footprint_field_properties(footprint, field)
+
+        if footprint["Name"] != footprint["Value"]["Value"]:
+            _log_error(footprint["Name"], "the name is not the same as the value")
+
+        if footprint["Reference"]["Value"] != "REF**":
+            _log_error(footprint["Name"], "the reference value must be 'REF**'")
+
+        _check_footprint_attributes(footprint)
 
 
 if __name__ == "__main__":
 
     check_symbols()
+    check_footprints()
