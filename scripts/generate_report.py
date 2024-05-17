2948f363c92c6e0abf205d1e79ac5ef2dc51f7d7
Danny <danny@lilytronics.nl>
2024-05-17 09:08:53 +0200

    add footprints to the report

--- scripts/generate_report.py
+++ scripts/generate_report.py
@@ -14,30 +14,46 @@ def generate_report():
     script_path = os.path.dirname(__file__)
     template_filename = os.path.abspath(os.path.join(script_path, "templates", "report_template.html"))
     output_filename = os.path.abspath(os.path.join(script_path, "..", "documents", "symbols_report.html"))
-    symbols = LibParser.get_symbols()
     print("\nGenerate report")
+
+    # Symbols
     generic_symbols_data = ""
     parts_data = ""
-    fields = []
+    symbol_fields = []
+    symbols = LibParser.get_symbols()
     for symbol in sorted(symbols, key=lambda x: x["Name"]):
         symbol_data = "{ "
         # Mandatory fields first
         for property_name in LibParser.SYMBOL_MANDATORY_FIELDS:
             value = html.escape(symbol[property_name])
             symbol_data += f'{property_name}: "{value}", '
-            if property_name not in fields:
-                fields.append(property_name)
+            if property_name not in symbol_fields:
+                symbol_fields.append(property_name)
         # Add non-mandatory fields
         for property_name in filter(lambda x: x not in LibParser.SYMBOL_MANDATORY_FIELDS, symbol):
             value = html.escape(symbol[property_name])
             symbol_data += f'{property_name}: "{value}", '
-            if property_name not in fields:
-                fields.append(property_name)
+            if property_name not in symbol_fields:
+                symbol_fields.append(property_name)
         symbol_data = symbol_data[:-2] + " }"
         if symbol["Extends"] == "":
             generic_symbols_data += f"    {symbol_data},\n"
         else:
             parts_data += f"    {symbol_data},\n"
+
+    # Footprints
+    footprint_fields = ["Name", "Revision", "Model"]
+    footprints_data = ""
+    footprints = LibParser.get_footprints()
+    for footprint in sorted(footprints, key=lambda x: x["Name"]):
+        footprint_data = "{ "
+        for field in footprint_fields:
+            value = footprint[field]
+            if isinstance(value, dict):
+                value = value["Value"]
+            footprint_data += f'{field}: "{html.escape(value)}", '
+        footprint_data = footprint_data[:-2] + " }"
+        footprints_data += f"    {footprint_data},\n"
 
     with open(template_filename, "r") as fp:
         template = Template(fp.read())
@@ -45,10 +61,13 @@ def generate_report():
     with open(output_filename, "w") as fp:
         fp.write(template.substitute(
             timestamp=datetime.now().strftime("%Y-%m-%d %H:%M"),
-            total=len(symbols),
-            fields=str(fields).replace("'", '"'),
+            total_symbols=len(symbols),
+            total_footprints=len(footprints),
+            symbol_fields=str(symbol_fields).replace("'", '"'),
+            footprint_fields=str(footprint_fields).replace("'", '"'),
             generic_symbols_data=generic_symbols_data[:-2],
-            parts_data=parts_data[:-2]
+            parts_data=parts_data[:-2],
+            footprints_data=footprints_data[:-2]
         ))
     print("\nDone")
 
