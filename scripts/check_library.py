21ad99a3b2b56bf80d89e8e9d70abc524943712b
Danny <danny@lilytronics.nl>
2024-05-26 08:36:18 +0200

    add show table with messages

--- scripts/check_library.py
+++ scripts/check_library.py
@@ -11,10 +11,28 @@ def check_library(messages):
     FootprintsChecker.run(messages)
 
 
+def _show_report(messages):
+    print("")
+    column_widths = [0, 0]
+    if len(messages) > 0:
+        for message in messages:
+            if len(message["item"]) > column_widths[0]:
+                column_widths[0] = len(message["item"])
+            if len(message["message"]) > column_widths[1]:
+                column_widths[1] = len(message["message"])
+        format_string = f"| {{item:{column_widths[0]}}} | {{message:{column_widths[1]}}} |"
+        row_line = f"+-{"-" * column_widths[0]}-+-{"-" * column_widths[1]}-+"
+        print(row_line)
+        print(format_string.format(item="item", message="message"))
+        print(row_line)
+        for message in messages:
+            print(format_string.format(item=message["item"], message=message["message"]))
+        print(row_line)
+    print(f"{len(messages)} messages")
+
+
 if __name__ == "__main__":
 
     report_messages = []
     check_library(report_messages)
-    for message in report_messages:
-        print(message["item"], message["message"])
-    print(f"{len(report_messages)} messages")
+    _show_report(report_messages)
