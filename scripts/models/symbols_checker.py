570fe7408067441b011977d2cd1153f9682cb9f8
Danny <danny.v.der.pol@gmail.com>
2025-05-14 21:19:41 +0200

    add exception in check empty field for special symbols

--- scripts/models/symbols_checker.py
+++ scripts/models/symbols_checker.py
@@ -82,9 +82,11 @@ class SymbolsChecker:
 
     @classmethod
     def _check_symbol_field_empty(cls, symbol_data, field_name, report_messages):
-        caller = f"({cls.__name__}._check_reference)"
+        caller = f"({cls.__name__}._check_symbol_field_empty)"
+        # Skip special symbols
+        no_check = "con" in symbol_data["Name"] and "cable_to_pcb" in symbol_data["Name"]
         # Ignore fields that are allowed to be empty or already have been checked
-        if field_name not in cls.SKIP_FIELDS:
+        if field_name not in cls.SKIP_FIELDS and not no_check:
             field_checked = False
             is_empty = False
             is_not_empty = False
