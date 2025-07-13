df65c6e803f294a9316c08be3fad0b93088dc683
Danny <danny.v.der.pol@gmail.com>
2025-07-13 20:25:29 +0200

    initialize tree with checks

--- toolbox/controllers/controller_check_library.py
+++ toolbox/controllers/controller_check_library.py
@@ -2,6 +2,10 @@
 Check library controller.
 """
 
+import wx
+
+from toolbox.models.id_manager import IdManager
+from toolbox.models.symbols_checker import SymbolsChecker
 from toolbox.views.view_check_library import ViewCheckLibrary
 
 
@@ -9,8 +13,21 @@ class ControllerCheckLibrary:
 
     name = "Check Libraries"
 
+    _checks = {
+        "Check symbols" : SymbolsChecker
+    }
+
     def __init__(self, parent):
         self._view = ViewCheckLibrary(parent)
+        self._view.initialize_tree(list(self._checks.keys()))
+        self._view.Bind(wx.EVT_BUTTON, self._on_check_click, id=IdManager.ID_BTN_CHECK)
+
+    ##################
+    # Event handlers #
+    ##################
+
+    def _on_check_click(self, event):
+        print("Run checks")
 
     ##########
     # Public #
--- toolbox/views/view_check_library.py
+++ toolbox/views/view_check_library.py
@@ -5,6 +5,8 @@ View for the check library tab
 import wx
 import wx.dataview
 
+from toolbox.models.id_manager import IdManager
+
 
 class ViewCheckLibrary(wx.Panel):
 
@@ -13,17 +15,31 @@ class ViewCheckLibrary(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent)
 
-        self._tree = wx.dataview.TreeListCtrl(self, wx.ID_ANY)
-        self._tree.AppendColumn("Check")
+        self._tree = wx.dataview.TreeListCtrl(self)
+        self._tree.AppendColumn("Checks")
         self._tree.AppendColumn("Result")
 
-        btn_run_checks = wx.Button(self, wx.ID_ANY, "Run checks")
+        btn_run_checks = wx.Button(self, IdManager.ID_BTN_CHECK, "Run checks")
 
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self._tree, 1, wx.EXPAND | wx.ALL, self._GAP)
         sizer.Add(btn_run_checks, 0, wx.ALL, self._GAP)
 
         self.SetSizer(sizer)
+
+    ##########
+    # Public #
+    ##########
+
+    def initialize_tree(self, checks):
+        self._tree.DeleteAllItems()
+        root = self._tree.GetRootItem()
+        for check in checks:
+            self._tree.AppendItem(root, check)
+            # self._tree.SetItemText(child, 1, "2 messages")
+            # for i in range(2):
+            #     msg = self._tree.AppendItem(child, f"Line {i}")
+            #     self._tree.SetItemText(msg, 1, f"This is message {i}")
 
 
 if __name__ == "__main__":
