3cc0c35718e182e93d54064657e1991871d61207
Danny <danny.v.der.pol@gmail.com>
2025-07-15 22:00:09 +0200

    add app data class

---
+++ toolbox/app_data.py
@@ -0,0 +1,27 @@
+"""
+Application data
+"""
+
+import os
+import sys
+
+
+class AppData:
+    APP_NAME = "Lily KiCad Toolbox"
+    VERSION = "1.0"
+    EXE_NAME = "LilyKiCadToolbox"
+    COMPANY = "LilyTronics"
+    # Application path depends on if run from script or from the executable
+    if EXE_NAME in sys.executable:
+        APP_PATH = os.path.dirname(sys.executable)
+    else:
+        APP_PATH = os.path.dirname(__file__)
+    APP_PATH = os.path.dirname(APP_PATH)
+
+
+if __name__ == "__main__":
+
+    print("App name   :", AppData.APP_NAME)
+    print("App version:", AppData.VERSION)
+    print("Exe name   :", AppData.EXE_NAME)
+    print("App folder :", AppData.APP_PATH)
