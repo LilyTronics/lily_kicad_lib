f2c6aa960f7593a28b363886b714feddf63f65d5
Danny <danny.v.der.pol@gmail.com>
2025-10-22 19:16:35 +0200

    build toolbox V3.14

--- toolbox/app_data.py
+++ toolbox/app_data.py
@@ -8,7 +8,7 @@ import sys
 
 class AppData:
     APP_NAME = "Lily KiCad Toolbox"
-    VERSION = "3.13"
+    VERSION = "3.14"
     EXE_NAME = "LilyKiCadToolbox"
     COMPANY = "LilyTronics"
     # Application path depends on if run from script or from the executable
