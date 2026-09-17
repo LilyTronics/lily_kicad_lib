62b2d6e6941fac06a1f0cfcafafc4ffeaaecebcb
Danny <danny.v.der.pol@gmail.com>
2026-09-17 19:37:42 +0200

    change quotes

--- toolbox/tool_runner/app_data.py
+++ toolbox/tool_runner/app_data.py
@@ -8,10 +8,10 @@ import sys
 from toolbox.tool_runner.src.os_specifics import get_user_data_dir
 
 
-APP_NAME = "Lily KiCad Toolbox"
-APP_VERSION = "4.0"
-EXE_NAME = "LilyKiCadToolbox"
-COMPANY  = "LilyTronics"
+APP_NAME = 'Lily KiCad Toolbox'
+APP_VERSION = '4.0'
+EXE_NAME = 'LilyKiCadToolbox'
+COMPANY  = 'LilyTronics'
 SHOW_TEMPLATE = False
 
 # Application path depends on if run from script or from the executable
@@ -24,15 +24,15 @@ else:
     # Show the template tool when run from IDE
     SHOW_TEMPLATE = True
 
-SETTINGS_FILE = os.path.join(get_user_data_dir(), EXE_NAME, f"{EXE_NAME}.json")
-TOOLS_PATH = os.path.join(APP_PATH, "tools")
-LOG_FILENAME = os.path.join(APP_PATH, f"{EXE_NAME}.log")
+SETTINGS_FILE = os.path.join(get_user_data_dir(), EXE_NAME, f'{EXE_NAME}.json')
+TOOLS_PATH = os.path.join(APP_PATH, 'tools')
+LOG_FILENAME = os.path.join(APP_PATH, f'{EXE_NAME}.log')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    print("App path     :", APP_PATH)
-    print("Settings file:", SETTINGS_FILE)
-    print("Tools path   :", TOOLS_PATH)
-    print("Log filename :", LOG_FILENAME)
-    print("Show template:", SHOW_TEMPLATE)
+    print('App path     :', APP_PATH)
+    print('Settings file:', SETTINGS_FILE)
+    print('Tools path   :', TOOLS_PATH)
+    print('Log filename :', LOG_FILENAME)
+    print('Show template:', SHOW_TEMPLATE)
