b2a37cfe19b88c89584e86003aaaebc8eb282e52
Danny <danny.v.der.pol@gmail.com>
2026-09-17 19:30:41 +0200

    change quotes

--- toolbox/tool_runner/src/os_specifics.py
+++ toolbox/tool_runner/src/os_specifics.py
@@ -11,15 +11,15 @@ from pathlib import Path
 def get_user_data_dir():
     extend_path = []
     match sys.platform:
-        case "win32":
-            extend_path = ["AppData", "Roaming"]
-        case "darwin":
-            extend_path = ["Library", "Application Support"]
+        case 'win32':
+            extend_path = ['AppData', 'Roaming']
+        case 'darwin':
+            extend_path = ['Library', 'Application Support']
         case _:  # Linux and others
-            extend_path = [".local", "share"]
+            extend_path = ['.local', 'share']
     return os.path.join(Path.home(), *extend_path)
 
 
-if __name__ =="__main__":
+if __name__ =='__main__':
 
-    print("User data dir:", get_user_data_dir())
+    print('User data dir:', get_user_data_dir())
